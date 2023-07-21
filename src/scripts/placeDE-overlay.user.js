c6d1691b8fc9e9e281484a0e38acd5adfa4cf0ca
bits0rcerer <25325997+bits0rcerer@users.noreply.github.com>
2023-07-21 18:51:53 +0200

    adapt to all canvas sizes

--- src/scripts/placeDE-overlay.user.js
+++ src/scripts/placeDE-overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/placeDE Template
 // @namespace    http://tampermonkey.net/
-// @version      8.5
+// @version      8.6
 // @description  try to take over the canvas!
 // @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
@@ -14,10 +14,24 @@ var overlayImage = null;
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
         const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
+        const canvas = canvasContainer.querySelector("canvas");
         overlayImage = document.createElement("img");
         updateImage();
-        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2500px;height: 2000px;pointerEvents: 'none';`;
+        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;pointerEvents: 'none';`;
         canvasContainer.appendChild(overlayImage);
+
+        const canvasObserver = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (mutation.type === "attributes") {
+                    overlayImage.style.width = mutation.target.getAttribute("width") + "px";
+                    overlayImage.style.height = mutation.target.getAttribute("height") + "px";
+                }
+            });
+        });
+
+        canvasObserver.observe(canvas, {
+            attributes: true
+        });
     }, false);
 }
 
